6d3df312745097a73ee7d488454c64346b1ae442
김윤서 <yo26on@soongsil.ac.kr>
2022-10-12 20:40:03 +0900

    #1 구조 변경

---
+++ uckgisagi-iOS/uckgisagi-iOS/External/Firebase/FirebaseService.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseService.swift
+//  uckgisagi-iOS
+//
+//  Created by 김윤서 on 2022/10/12.
+//
+
+import Foundation
---
+++ uckgisagi-iOS/uckgisagi-iOS/Network/API.swift
@@ -0,0 +1,8 @@
+//
+//  API.swift
+//  uckgisagi-iOS
+//
+//  Created by 김윤서 on 2022/10/12.
+//
+
+import Foundation
