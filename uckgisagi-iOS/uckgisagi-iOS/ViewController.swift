0c62c3f6141ec9b98942a321b4b84f08ce565481
김윤서 <yo26on@soongsil.ac.kr>
2022-10-12 20:23:39 +0900

    #1 프로젝트 초기 설정

---
+++ uckgisagi-iOS/uckgisagi-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uckgisagi-iOS
+//
+//  Created by 김윤서 on 2022/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
