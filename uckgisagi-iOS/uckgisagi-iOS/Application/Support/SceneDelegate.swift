463af5a61bdb1e865a4bc2f6b001f54b519dbb2e
Yoonseo Kim <yo26on@soongsil.ac.kr>
2022-10-27 23:30:43 +0900

    #1 RootSwitcher 적용

--- uckgisagi-iOS/uckgisagi-iOS/Application/Support/SceneDelegate.swift
+++ uckgisagi-iOS/uckgisagi-iOS/Application/Support/SceneDelegate.swift
@@ -14,8 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
+        RootSwitcher.update(.splash)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
