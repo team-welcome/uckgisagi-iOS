89482fb004c812b554ab6968db392f73d4d5233f
Yoonseo Kim <yo26on@soongsil.ac.kr>
2022-10-27 23:28:27 +0900

    #1 RootSwitcher 구현

---
+++ uckgisagi-iOS/uckgisagi-iOS/Global/Util/RootSwitcher.swift
@@ -0,0 +1,36 @@
+//
+//  RootSwitcher.swift
+//  uckgisagi-iOS
+//
+//  Created by 김윤서 on 2022/10/27.
+//
+
+import UIKit
+import RxSwift
+
+final class RootSwitcher {
+    enum Destination {
+        case splash
+        case login
+        case home
+        case custom(UIViewController)
+    }
+
+    static func update(_ destination: Destination) {
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
+        guard let delegate = sceneDelegate else {
+            return
+        }
+        switch destination {
+        case .splash:
+            delegate.window?.rootViewController = ViewController()
+        case .login:
+            delegate.window?.rootViewController = ViewController()
+        case .home:
+            delegate.window?.rootViewController = ViewController()
+        /// 테스트에만 사용할것
+        case let .custom(viewController):
+            delegate.window?.rootViewController = viewController
+        }
+    }
+}
