a321b87b1b38f2f7e2e480200f3947463a9d44d2
SojinLee <513sojin@naver.com>
2022-10-29 23:04:12 +0900

    #9 홈 로고 이미지 및 아이콘 추가

--- uckgisagi-iOS/uckgisagi-iOS/Global/Resource/Image.swift
+++ uckgisagi-iOS/uckgisagi-iOS/Global/Resource/Image.swift
@@ -13,6 +13,14 @@ enum Image {
     static let btnFollowing = UIImage(named: "btn.following")!
     static let imgAddUser = UIImage(named: "img.addUser")!
     static let icBack = UIImage(named: "ic.back")!
+    
+    // MARK: - Login
     static let bigLogo = UIImage(named: "big.logo")!
     static let appleLogo = UIImage(named: "apple.logo")!
+    
+    // MARK: - Home
+    static let smallLogo = UIImage(named: "small.logo")!
+    static let icSurround = UIImage(named: "ic.surround")!
+    static let icCircleTap = UIImage(named: "ic.circle.tap")!
+    static let icCircle = UIImage(named: "ic.circle")!
 }
