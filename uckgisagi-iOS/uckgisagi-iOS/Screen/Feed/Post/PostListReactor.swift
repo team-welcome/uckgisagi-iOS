8cf94ccf0aa50da00cd0f67b3c04b3a665e0eb4a
Yoonseo Kim <yo26on@soongsil.ac.kr>
2022-11-01 00:36:16 +0900

    #10 PostDTO 분리

--- uckgisagi-iOS/uckgisagi-iOS/Screen/Feed/Post/PostListReactor.swift
+++ uckgisagi-iOS/uckgisagi-iOS/Screen/Feed/Post/PostListReactor.swift
@@ -7,30 +7,6 @@
 
 import ReactorKit
 
-struct PostDTO: Decodable {
-    let id: Int
-    let content: String
-    let isLiked: Bool
-    let imageURL: String
-}
-
-extension PostDTO: Equatable, Hashable {
-    static func == (lhs: PostDTO, rhs: PostDTO) -> Bool {
-        return lhs.id == rhs.id
-    }
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-
-    static func isSameContents(lhs: PostDTO, rhs: PostDTO) -> Bool {
-        return lhs.id == rhs.id
-        && lhs.content == rhs.content
-        && lhs.isLiked == rhs.isLiked
-        && lhs.imageURL == rhs.imageURL
-    }
-}
-
 final class PostListReactor: Reactor {
     enum ReactorType {
         case all
