26449dd7a527804673f6e62d142becfc5b0d69e8
Yoonseo Kim <yo26on@soongsil.ac.kr>
2022-10-31 23:44:21 +0900

    #10 PostListDataSource

---
+++ uckgisagi-iOS/uckgisagi-iOS/Screen/Feed/Post/PostListDataSource.swift
@@ -0,0 +1,103 @@
+//
+//  PostListDataSource.swift
+//  uckgisagi-iOS
+//
+//  Created by 김윤서 on 2022/10/31.
+//
+
+import UIKit
+
+import RxSwift
+
+final class PostListDataSource {
+    // MARK: - typealias
+    typealias PostCell = PostCollectionViewCell
+    typealias CellProvider = (UICollectionView, IndexPath, Int) -> UICollectionViewCell?
+    typealias DiffableDataSource = UICollectionViewDiffableDataSource<Section, Int>
+    typealias CellRegistration<Cell: UICollectionViewCell> = UICollectionView.CellRegistration<Cell, Int>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Int>
+
+    // MARK: - Properties
+    private lazy var dataSource = createDataSource()
+    private let collectionView: UICollectionView
+
+    private var posts: [Int: PostDTO]
+
+    enum Section {
+        case main
+    }
+
+    init(collectionView: UICollectionView) {
+        self.collectionView = collectionView
+        self.posts = .init()
+    }
+
+    private func createDataSource() -> DiffableDataSource {
+        let commentRegistration: CellRegistration<PostCell> = configureRegistration()
+        let cellProvider: CellProvider = { collectionView, indexPath, item in
+            return collectionView.dequeueConfiguredReusableCell(
+                using: commentRegistration,
+                for: indexPath,
+                item: item
+            )
+        }
+        return UICollectionViewDiffableDataSource<Section, Int> (
+            collectionView: collectionView,
+            cellProvider: cellProvider
+        )
+    }
+
+    private func configureRegistration<Cell: PostCell>() -> CellRegistration<Cell> {
+        return CellRegistration<Cell> { [weak self] cell, indexPath, postID in
+            guard
+                let self = self,
+                let post = self.posts[postID]
+            else { return }
+
+            cell.configure(
+                content: post.content,
+                imageURL: post.imageURL,
+                isSelected: post.isLiked
+            )
+        }
+    }
+
+    func update(posts: [PostDTO]) {
+        var snapshot = Snapshot()
+        if !snapshot.sectionIdentifiers.contains(.main) {
+            snapshot.appendSections([.main])
+        }
+
+        let ids = posts.map { $0.id }.uniqued()
+        posts
+            .filter { !self.posts.keys.contains($0.id) }
+            .forEach { self.posts[$0.id] = $0 }
+        snapshot.appendItems(ids)
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+
+    func reloadIfNeeded(item: PostDTO) {
+        guard
+            posts.keys.contains(item.id),
+            let oldValue = posts.updateValue(item, forKey: item.id),
+            PostDTO.isSameContents(lhs: oldValue, rhs: item) == false
+        else { return }
+        var currentSnapshot = dataSource.snapshot()
+        currentSnapshot.reloadItems([item.id])
+        dataSource.apply(currentSnapshot, animatingDifferences: false)
+    }
+
+    func itemIdentifier(for indexPath: IndexPath) -> PostDTO? {
+        guard let postID = dataSource.itemIdentifier(for: indexPath) else { return nil }
+        return posts[postID]
+    }
+
+
+}
+
+extension Sequence where Element: Hashable {
+    func uniqued() -> [Element] {
+        var set = Set<Element>()
+        return filter { set.insert($0).inserted }
+    }
+}
