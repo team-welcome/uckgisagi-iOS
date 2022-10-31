4f3f2a947a7c3e62c656aee210dad70ddfd66331
SojinLee <513sojin@naver.com>
2022-10-31 22:29:30 +0900

    #9 - userProfileCollectionView 추가

--- uckgisagi-iOS/uckgisagi-iOS/Screen/Home/HomeViewController.swift
+++ uckgisagi-iOS/uckgisagi-iOS/Screen/Home/HomeViewController.swift
@@ -16,6 +16,27 @@ class HomeViewController: BaseViewController {
     private let surroundButton = UIButton()
     private let homeScrollView = UIScrollView()
     private let testView1 = UIView()
+    private var userProfileCollectionView: UICollectionView = {
+        var layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = .zero
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.contentInset = UIEdgeInsets(top: .zero, left: 20, bottom: .zero, right: .zero)
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        userProfileCollectionView.delegate = self
+        userProfileCollectionView.dataSource = self
+        userProfileCollectionView.register(UserProfileCollectionViewCell.self, forCellWithReuseIdentifier: UserProfileCollectionViewCell.identifier)
+    }
     
     override func setLayouts() {
         navigationView.snp.makeConstraints {
@@ -38,12 +59,18 @@ class HomeViewController: BaseViewController {
         }
         
         homeScrollView.snp.makeConstraints {
-            $0.top.equalTo(navigationView.snp.bottom)
+            $0.top.equalTo(userProfileCollectionView.snp.bottom)
             $0.leading.trailing.bottom.equalToSuperview()
         }
         
+        userProfileCollectionView.snp.makeConstraints {
+            $0.top.equalTo(navigationView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(54)
+        }
+        
         testView1.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(30)
+            $0.top.equalTo(userProfileCollectionView.snp.bottom).offset(10)
             $0.width.equalTo(360)
             $0.height.equalTo(1200)
             $0.centerX.equalToSuperview()
@@ -53,11 +80,29 @@ class HomeViewController: BaseViewController {
     
     override func setProperties() {
         navigationView.addSubviews(ukgisagiLogo, surroundButton)
-        view.addSubviews(navigationView, homeScrollView)
+        view.addSubviews(navigationView, userProfileCollectionView, homeScrollView)
         homeScrollView.addSubviews(testView1)
         
         ukgisagiLogo.image = Image.bigLogo
         surroundButton.setImage(Image.icSurround, for: .normal)
         testView1.backgroundColor = .blue
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8 // MARK: - 서버에서 받는 값으로 수정하기
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UserProfileCollectionViewCell.identifier, for: indexPath) as? UserProfileCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(name: "뭉", isFriend: false) // MARK: - 서버에서 받는 값으로 수정하기
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 48, height: 54)
     }
 }
