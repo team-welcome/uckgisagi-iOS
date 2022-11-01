ddb67b9d2bf7b19c02c4c1530841757a67aacf6e
Yoonseo Kim <yo26on@soongsil.ac.kr>
2022-11-02 00:48:27 +0900

    #12 ShopListView 추가

---
+++ uckgisagi-iOS/uckgisagi-iOS/Screen/Feed/Shop/View/ShopListView.swift
@@ -0,0 +1,132 @@
+//
+//  ShopListView.swift
+//  uckgisagi-iOS
+//
+//  Created by 김윤서 on 2022/11/01.
+//
+
+import UIKit
+
+import SnapKit
+
+final class ShopListView: UIView {
+    private(set) lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: self.bounds, collectionViewLayout: createLayout())
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
+
+    enum Section: Int {
+        case hot = 0
+        case all = 1
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayouts()
+    }
+
+    private func setLayouts() {
+        addSubview(collectionView)
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    private func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { [weak self] index, _ in
+            switch Section(rawValue: index) {
+            case .hot:
+                return self?.hotSectionLayout()
+            case .all:
+                return self?.allSectionLayout()
+            default:
+                return self?.hotSectionLayout()
+            }
+        }
+        return layout
+    }
+
+    private func hotSectionLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(
+            top: 0,
+            leading: 16,
+            bottom: 0,
+            trailing: 0
+        )
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.8),
+            heightDimension: .absolute(241)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+
+        let section = NSCollectionLayoutSection(group: group)
+        let headerSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(60)
+        )
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top
+        )
+        section.boundarySupplementaryItems = [header]
+        section.orthogonalScrollingBehavior = .groupPaging
+        section.contentInsets = .init(
+            top: .zero,
+            leading: .zero,
+            bottom: .zero,
+            trailing: 16
+        )
+        return section
+    }
+
+    private func allSectionLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .absolute(103)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(
+            top: .zero,
+            leading: 16,
+            bottom: .zero,
+            trailing: 16
+        )
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .absolute(103)
+        )
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 12
+        let headerSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(60)
+        )
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top
+        )
+        section.boundarySupplementaryItems = [header]
+
+        return section
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
