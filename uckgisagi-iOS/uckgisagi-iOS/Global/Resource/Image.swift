d2096be311a334dc26af2cc94dd0cb7028d942c2
Yoonseo Kim <yo26on@soongsil.ac.kr>
2022-11-02 01:18:39 +0900

    #12 이미지 추가

--- uckgisagi-iOS/uckgisagi-iOS/Global/Resource/Image.swift
+++ uckgisagi-iOS/uckgisagi-iOS/Global/Resource/Image.swift
@@ -8,18 +8,8 @@
 import UIKit.UIImage
 
 enum Image {
+    // MARK: - Icon
     static let icSearch = UIImage(named: "ic.search")!
-    static let btnFollow = UIImage(named: "btn.follow")!
-    static let btnFollowing = UIImage(named: "btn.following")!
-    static let imgAddUser = UIImage(named: "img.addUser")!
-    static let icBack = UIImage(named: "ic.back")!
-    
-    // MARK: - Login
-    static let bigLogo = UIImage(named: "big.logo")!
-    static let appleLogo = UIImage(named: "apple.logo")!
-    
-    // MARK: - Home
-    static let smallLogo = UIImage(named: "small.logo")!
     static let icSurround = UIImage(named: "ic.surround")!
     static let icCircleTap = UIImage(named: "ic.circle.tap")!
     static let icCircle = UIImage(named: "ic.circle")!
@@ -27,6 +17,24 @@ enum Image {
     static let icSprout = UIImage(named: "ic.sprout")!
     static let icHuk = UIImage(named: "ic.huk")!
     static let icPost = UIImage(named: "ic.post")!
-    static let postTestImage = UIImage(named: "postTestImage")!
+    static let icBack = UIImage(named: "ic.back")!
     static let icMark = UIImage(named: "ic.mark")!
+
+    // MARK: - Btn
+    static let btnFollow = UIImage(named: "btn.follow")!
+    static let btnFollowing = UIImage(named: "btn.following")!
+
+    // MARK: - Logo
+    static let bigLogo = UIImage(named: "big.logo")!
+    static let appleLogo = UIImage(named: "apple.logo")!
+    static let smallLogo = UIImage(named: "small.logo")!
+
+    // MARK: - Navigation
+    static let imgAddUser = UIImage(named: "img.addUser")!
+
+    // MARK: - Dummy
+    static let postTestImage = UIImage(named: "postTestImage")!
+    static let imgDummy2 = UIImage(named: "img.dummy2")!
+    static let imgDummy3 = UIImage(named: "img.dummy3")!
+    static let imgDummy4 = UIImage(named: "img.dummy4")!
 }
