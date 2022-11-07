993b88ce647fe56799b32856b3b6d298e4dc5810
SojinLee <513sojin@naver.com>
2022-11-08 02:01:43 +0900

    #15 작성된 post가 없을 때의 cell 추가

---
+++ uckgisagi-iOS/uckgisagi-iOS/Screen/Home/Cell/EmptyPostTableViewCell.swift
@@ -0,0 +1,72 @@
+//
+//  EmptyPostTableViewCell.swift
+//  uckgisagi-iOS
+//
+//  Created by Sojin Lee on 2022/11/07.
+//
+
+import UIKit
+
+import SnapKit
+
+class EmptyPostTableViewCell: UITableViewCell {
+    static let identifier = "EmptyPostTableViewCell"
+    let icHuk: UIImageView = .init()
+    let noticeLabel: UILabel = .init()
+    let pushButton: UIButton = .init()
+    let stackView: UIStackView = .init()
+
+    // MARK: - Initailize
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setProperties()
+        setLayouts()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setProperties() {
+        stackView.addArrangedSubviews(icHuk, noticeLabel, pushButton)
+        addSubview(stackView)
+        
+        icHuk.image = Image.icHuk
+        pushButton.setImage(Image.btnPush, for: .normal)
+        noticeLabel.text = "아직 작성된 인증글이 없어요"
+        noticeLabel.textColor = Color.mediumGray
+        noticeLabel.font = .systemFont(ofSize: 15, weight: .regular)
+        
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+    }
+    
+    private func setLayouts() {
+        icHuk.snp.makeConstraints {
+            $0.width.height.equalTo(160)
+        }
+        
+        pushButton.snp.makeConstraints {
+            $0.width.equalTo(96)
+            $0.height.equalTo(40)
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.width.height.equalTo(250)
+            $0.centerX.centerY.equalToSuperview()
+            
+        }
+    }
+    
+    func configure(isFriend: Bool) {
+        if isFriend {
+            pushButton.isHidden = true
+            
+            stackView.snp.updateConstraints {
+                $0.height.equalTo(200)
+            }
+        }
+    }
+}
