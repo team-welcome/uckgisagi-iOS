c2e5f1000524ceace4a5d51a69202978ac8a87ab
Yoonseo Kim <yo26on@soongsil.ac.kr>
2022-11-21 13:17:23 +0900

    #32 InterCeptor 구현

--- uckgisagi-iOS/uckgisagi-iOS/Network/Interceptor.swift
+++ uckgisagi-iOS/uckgisagi-iOS/Network/Interceptor.swift
@@ -12,33 +12,92 @@ import Moya
 import RxSwift
 
 final class Interceptor : RequestInterceptor {
-    private let disposeBag = DisposeBag()
+    static let shared = Interceptor()
 
-    func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
-        guard
-            let response = request.task?.response as? HTTPURLResponse,
-            response.statusCode == 401
-        else {
-            completion(.doNotRetry)
-            return
-        }
+    private init() {}
 
-        NetworkService.shared.auth.reissue()
-            .subscribe(onNext: { response in
-                guard
-                    let accessToken = response.data?.accessToken,
-                    let refreshToken = response.data?.refreshToken
-                else {
-                    KeychainHandler.shared.removeAll()
-                    UserDefaultHandler.shared.removeAll()
-                    RootSwitcher.update(.login)
-                    return completion(.doNotRetry)
-                }
-                KeychainHandler.shared.accessToken = accessToken
-                KeychainHandler.shared.refreshToken = refreshToken
-                completion(.retryWithDelay(0.1))
-            })
-            .disposed(by: disposeBag)
+    private var retryLimit = 2
+
+    func adapt(
+        _ urlRequest: URLRequest,
+        for session: Session,
+        completion: @escaping (Result<URLRequest, Error>) -> Void
+    ) {
+        var urlRequest = urlRequest
+        urlRequest.setValue(
+            "Bearer " + KeychainHandler.shared.accessToken,
+            forHTTPHeaderField: "Authorization"
+        )
+        completion(.success(urlRequest))
     }
 
+    func retry(
+        _ request: Request,
+        for session: Session,
+        dueTo error: Error,
+        completion: @escaping (RetryResult) -> Void
+    ) {
+        guard
+            let statusCode = request.response?.statusCode,
+            request.retryCount < retryLimit,
+            statusCode == 401
+        else {
+            return completion(.doNotRetry)
+        }
+
+        refreshToken { isSuccess in
+            isSuccess ? completion(.retry) : completion(.doNotRetry)
+        }
+    }
+
+    private func refreshToken(completion: @escaping (Bool) -> Void) {
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer \(KeychainHandler.shared.accessToken)"
+        ]
+
+        let parameter: Parameters = [
+            "accessToken": KeychainHandler.shared.accessToken,
+            "refreshToken": KeychainHandler.shared.refreshToken
+        ]
+
+        let dataTask = AF.request(
+            Environment.baseURL + "/v1/auth/reissue",
+            method: .post,
+            parameters: parameter,
+            encoding: JSONEncoding.default,
+            headers: headers
+        )
+
+        dataTask.responseData { responseData in
+            switch responseData.result {
+            case .success:
+                guard
+                    let value = responseData.value,
+                    let decodedData = try? JSONDecoder().decode(BaseResponse<TokenDTO>.self, from: value),
+                    let statusCode = decodedData.statusCode,
+                    let data = decodedData.data,
+                    statusCode == .okay || statusCode == .created
+                else {
+                    KeychainHandler.shared.refreshToken = ""
+                    KeychainHandler.shared.accessToken = ""
+                    RootSwitcher.update(.login)
+                    return completion(false)
+                }
+                dump(decodedData)
+
+                KeychainHandler.shared.refreshToken = data.refreshToken
+                KeychainHandler.shared.accessToken = data.accessToken
+
+                completion(true)
+
+            case .failure(_):
+                KeychainHandler.shared.refreshToken = ""
+                KeychainHandler.shared.accessToken = ""
+                RootSwitcher.update(.login)
+                completion(false)
+            }
+
+        }
+    }
 }
